bbdd18475ee89259d967b669c22d8b8cb4caa48b
Kantarakorn Suwanrat <bass-0892009988@hotmail.com>
2018-05-08 15:24:19 +0700

    update data

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -2,7 +2,9 @@ package oopmit;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println( s: "Hello,My name is Kantarakorn");
+        System.out.println("Hello,My name is Kantarakorn");
+        System.out.println("Major: Information System");
+        System.out.println("Management Technolopy");
     }
 }
 
