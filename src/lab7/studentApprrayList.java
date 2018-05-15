a47e3c075f6c5c185948bdfb923df339c992ed3d
Kantarakorn <bass-0892009988@hotmail.com>
2018-05-15 15:49:38 +0700

    Put Object to ArrayList Class-edited

---
+++ src/lab7/studentApprrayList.java
@@ -0,0 +1,14 @@
+package lab7;
+
+public class sutdentApp {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Mit");
+        s1.setAge(22);
+        s1.setGander("Male");
+        s1.setHeight(180);
+        System.out.print(s1.toString());
+        System.s2 = new student (name: "Safe",age:90, gender:"Female", height:190);
+        System.out.print(s2.toString());
+    }
+}
